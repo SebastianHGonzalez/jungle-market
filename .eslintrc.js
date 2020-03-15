0f6334f40f306784cadf7ec65f736cebace05937
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2020-03-15 12:06:09 -0300

    Main package setup
    eslint setup
    docker compose setup
    deps setup

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    extends: ['airbnb', 'plugin:@typescript-eslint/recommended', "plugin:jest/recommended"],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'prettier', 'jest'],
+    settings: {
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+            typescript: {},
+        },
+    },
+    env: {
+        "jest/globals": true
+    },
+    rules: {
+        "jest/no-disabled-tests": [1],
+        "jest/no-focused-tests": [2],
+        "jest/no-identical-title": [2],
+        "jest/prefer-to-have-length": [1],
+        "jest/valid-expect": [2],
+        'import/extensions': [0],
+        'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.tsx', '**/test.ts'] }],
+        '@typescript-eslint/indent': [2, 2],
+    },
+};
---
+++ README.md
@@ -0,0 +1,16 @@
+# jungle-market
+
+event driven mini shopping system.
+
+## Prerequisites
+
+- Docker
+- Docker Compose
+
+## Development
+
+start services
+
+```bash
+docker-compose <start|up> [api]
+```
