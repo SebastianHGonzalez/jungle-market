56239fa3f1aceff50cf7c73facd31a7f64db90b0
Sebastian Gonzalez <sebastian.gonzalez@fravega.com.ar>
2020-04-11 16:39:05 -0300

    Updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,41 @@
 module.exports = {
-    extends: ['airbnb', 'plugin:@typescript-eslint/recommended', "plugin:jest/recommended"],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'prettier', 'jest'],
-    settings: {
-        'import/parsers': {
-            '@typescript-eslint/parser': ['.ts', '.tsx'],
-        },
-        'import/resolver': {
-            typescript: {},
-        },
+  extends: [
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier", "jest"],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
     },
-    env: {
-        "jest/globals": true
-    },
-    rules: {
-        "import/no-unresolved": [0],
-        "jest/no-disabled-tests": [1],
-        "jest/no-focused-tests": [2],
-        "jest/no-identical-title": [2],
-        "jest/prefer-to-have-length": [1],
-        "jest/valid-expect": [2],
-        'import/extensions': [0],
-        'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.tsx', '**/test.ts'] }],
-        '@typescript-eslint/indent': [2, 2],
-    },
+    "import/resolver": {
+      typescript: {}
+    }
+  },
+  env: {
+    "jest/globals": true
+  },
+  rules: {
+    "import/no-unresolved": [0],
+    "jest/no-disabled-tests": [1],
+    "jest/no-focused-tests": [2],
+    "jest/no-identical-title": [2],
+    "jest/prefer-to-have-length": [1],
+    "jest/valid-expect": [2],
+    "import/extensions": [0],
+    "import/no-extraneous-dependencies": [
+      2,
+      { devDependencies: ["**/test.tsx", "**/test.ts"] }
+    ],
+    "@typescript-eslint/indent": [2, 2],
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".jsx", ".tsx"]
+      }
+    ],
+    "@typescript-eslint/explicit-function-return-type": [0]
+  }
 };
