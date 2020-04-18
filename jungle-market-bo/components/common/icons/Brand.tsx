85eecc19de0dbaba504aa6e5f1c19914c590dc45
Sebastian Gonzalez <sebastian.gonzalez@fravega.com.ar>
2020-04-18 19:15:30 -0300

    Added logo

--- jungle-market-bo/components/common/icons/Brand.tsx
+++ jungle-market-bo/components/common/icons/Brand.tsx
@@ -1,14 +1,8 @@
-import React from 'react';
 import styled from 'styled-components';
 
-import Title from '../typography/Title';
-
-interface Props {
-  className?: string;
-}
-
-function Brand({ className }: Props) {
-  return <Title className={className}>Jungle Market</Title>;
-}
-
-export default styled(Brand)``;
+export default styled.img.attrs({
+  src: '/images/logo.svg',
+  alt: 'Jungle Market',
+})`
+  height: 3em;
+`;
