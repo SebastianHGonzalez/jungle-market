e293da91ca26a88c1e155c591a4402f2280df87a
Sebastian Gonzalez <sebastian.gonzalez@fravega.com.ar>
2020-04-21 10:22:51 -0300

    Connected command api to message queue

--- jungle-market-graphql-command-api/src/config/index.ts
+++ jungle-market-graphql-command-api/src/config/index.ts
@@ -1,6 +1,12 @@
+import { config } from 'dotenv';
+
+config();
+
 export default function getConfig() {
   return {
-    shoppingCartCommandService:
-      process.env.JUNGLE_MARKET_SHOPPING_CART_COMMAND_SERVICE_ENDPOINT,
+    amqpURL: process.env.JUNGLE_MARKET_AMQP_URL,
+    exchange: process.env.JUNGLE_MARKET_AMQP_EXCHANGE,
+    exchangeType: process.env.JUNGLE_MARKET_AMQP_EXCHANGE_TYPE,
+    routingKey: process.env.JUNGLE_MARKET_AMQP_ROUTING_KEY,
   };
 }
--- jungle-market-graphql-command-api/src/index.ts
+++ jungle-market-graphql-command-api/src/index.ts
@@ -1,31 +1,71 @@
-import {
-  introspectSchema,
-  makeRemoteExecutableSchema,
-  ApolloServer,
-} from 'apollo-server';
-import { HttpLink } from 'apollo-link-http';
-import fetch from 'node-fetch';
+import amqp from 'amqplib';
+import { ApolloServer, gql } from 'apollo-server';
 
 import getConfig from './config';
 
-(async () => {
-  const { shoppingCartCommandService } = getConfig();
+const {
+  amqpURL, exchange, exchangeType, routingKey,
+} = getConfig();
 
-  const link = new HttpLink({
-    uri: shoppingCartCommandService,
-    fetch: fetch as never,
+amqp
+  .connect(amqpURL)
+  .then((connection) => connection.createChannel().catch((e) => {
+    connection.close();
+    throw e;
+  }))
+  .then((channel) => {
+    channel.assertExchange(exchange, exchangeType, {
+      durable: false,
+    });
+
+    const typeDefs = gql`
+      type ClientPickedProductMutationResponse {
+        clientId: ID
+        skuId: ID
+      }
+
+      type Query {
+        dummy: String
+      }
+
+      type Mutation {
+        clientPickedProduct(
+          clientId: ID!
+          skuId: ID!
+        ): ClientPickedProductMutationResponse
+      }
+    `;
+
+    const resolvers = {
+      Query: {
+        dummy: () => 'ok',
+      },
+      Mutation: {
+        clientPickedProduct: (parent: unknown, args: any) => {
+          channel.publish(
+            exchange,
+            routingKey,
+            Buffer.from(
+              JSON.stringify({
+                type: 'clientPickedProduct',
+                clientId: args.clientId,
+                skuId: args.skuId,
+              }),
+            ),
+          );
+
+          return {
+            clientId: args.clientId,
+            skuId: args.skuId,
+          };
+        },
+      },
+    };
+
+    const server = new ApolloServer({ typeDefs, resolvers });
+
+    // The `listen` method launches a web server.
+    server.listen().then(({ url }) => {
+      console.log(`🚀  Server ready at ${url}`);
+    });
   });
-
-  const schema = await introspectSchema(link);
-
-  const executableSchema = makeRemoteExecutableSchema({
-    schema,
-    link,
-  });
-
-  const server = new ApolloServer({ schema: executableSchema });
-
-  server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
-})();
