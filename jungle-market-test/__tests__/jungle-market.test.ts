b669a2d5389e5832ad16081ca66a868b89f5cb08
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2020-06-10 11:44:21 -0300

    Fixed integration test for cutomer enters a branch use case

--- jungle-market-test/__tests__/jungle-market.test.ts
+++ jungle-market-test/__tests__/jungle-market.test.ts
@@ -6,13 +6,16 @@ describe('jungle-market', () => {
   const customerId = 'customer1';
   const customerNonce = '1234567890dvahusjio';
 
-  it('runs', async () => {
+  test('when a customer enters a branch a shopping cart is created with his customer nonce', async () => {
     await customerEntersBranch(customerNonce, branchId);
 
     await new Promise((resolve) => { setTimeout(resolve, 500); });
 
     const { data: { shoppingCarts: { shoppingCarts } } } = await getShoppingCartsFromBranch(branchId);
-    expect(shoppingCarts).toHaveLength(1);
-    expect(shoppingCarts[0].customer.nonce).toBe(customerNonce);
+    const customerShoppingCart = shoppingCarts.find(
+      (shoppingCart: { customer: { nonce: string }; state: string }) => shoppingCart.customer.nonce === customerNonce && shoppingCart.state === 'OPEN'
+    );
+
+    expect(customerShoppingCart).toBeTruthy();
   });
 });
