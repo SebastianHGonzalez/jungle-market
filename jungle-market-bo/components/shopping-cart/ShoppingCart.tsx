fecd61901e5468643fda333ad5705b47f8429968
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2020-06-27 14:32:06 -0300

    Added shopping cart history to back office

--- jungle-market-bo/components/shopping-cart/ShoppingCart.tsx
+++ jungle-market-bo/components/shopping-cart/ShoppingCart.tsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 
 import { Title, SubTitle } from 'components/common/typography';
 import I18n from 'components/common/i18n';
+import {
+  Stepper, Step, StepIcon, StepContent, StepTitle, StepSubTitle, StepBody,
+} from 'components/common/stepper';
+import FormattedDate from 'components/common/format/FormattedDate';
 
 import useShoppingCart from 'hooks/useShoppingCart';
 
@@ -17,6 +21,16 @@ interface Props extends React.Props<never> {
 const ProductsList = styled.ul``;
 const ProductsListItem = styled.li``;
 
+const Layout = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(15em, 1fr));
+`;
+
+const Section = styled.section`
+  display: grid;
+  grid-gap: 1em;
+`;
+
 export default function ShoppingCart({
   id,
 }: Props) {
@@ -26,49 +40,90 @@ export default function ShoppingCart({
     customer,
     products,
     state,
+    history,
   } = data?.shoppingCart?.shoppingCart ?? {} as ShoppingCartType;
 
   return (
     <>
       <Title>
         <I18n id="shoppingCart.label" />
+        {': '}
+        {state && <ShoppingCartState state={state} />}
       </Title>
       {loading && 'loading'}
       {error && 'error'}
-      <SubTitle>
-        {state && <ShoppingCartState state={state} />}
-      </SubTitle>
-      <SubTitle>
-        {branch && (
-          <>
-            <I18n id="shoppingCart.branch.label" />
-            {': '}
-            {branch.cname}
-          </>
-        )}
-      </SubTitle>
-      <SubTitle>
-        <I18n id="shoppingCart.customer.label" />
-      </SubTitle>
-      {customer && (
-        <>
-          <I18n id="shoppingCart.customer.fullName.label" />
-          {': '}
-          {customer.fullName ?? <I18n id="shoppingCart.customer.unidentified.label" />}
-        </>
-      )}
-      <SubTitle>
-        <I18n id="shoppingCart.products.label" />
-      </SubTitle>
-      <ProductsList>
-        {products?.map(({ count, sku: { id: skuId, shortName } }) => (
-          <ProductsListItem key={skuId}>
-            {shortName}
-            {': '}
-            {count}
-          </ProductsListItem>
-        ))}
-      </ProductsList>
+
+      <Layout>
+        <Section>
+          <div>
+            <SubTitle>
+              {branch && (
+                <>
+                  <I18n id="shoppingCart.branch.label" />
+                  {': '}
+                  {branch.cname}
+                </>
+              )}
+            </SubTitle>
+          </div>
+
+          <div>
+            <SubTitle>
+              <I18n id="shoppingCart.customer.label" />
+            </SubTitle>
+            {customer && (
+              <>
+                <I18n id="shoppingCart.customer.fullName.label" />
+                {': '}
+                {customer.fullName ?? <I18n id="shoppingCart.customer.unidentified.label" />}
+              </>
+            )}
+          </div>
+
+          <div>
+            <SubTitle>
+              <I18n id="shoppingCart.products.label" />
+            </SubTitle>
+            <ProductsList>
+              {products?.map(({ count, sku: { id: skuId, shortName } }) => (
+                <ProductsListItem key={skuId}>
+                  {shortName}
+                  {': '}
+                  {count}
+                </ProductsListItem>
+              ))}
+            </ProductsList>
+          </div>
+        </Section>
+        <Section>
+
+          <Stepper>
+            {history?.map(({ createdAt, name, payload: { sku: { shortName } } }) => (
+              <Step key={createdAt}>
+                <StepIcon />
+                <StepContent>
+                  <StepTitle>
+                    <I18n id={`shoppingCart.history.${name}.label`} />
+                  </StepTitle>
+                  <StepSubTitle>
+                    <FormattedDate value={createdAt} />
+                    {' - '}
+                    <FormattedDate
+                      value={createdAt}
+                      options={{
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        second: '2-digit',
+                      }}
+                    />
+                  </StepSubTitle>
+                  <StepBody>{shortName}</StepBody>
+                </StepContent>
+              </Step>
+            ))}
+          </Stepper>
+        </Section>
+      </Layout>
     </>
   );
 }
